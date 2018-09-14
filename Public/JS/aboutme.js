80b2616a68b913ccbc4ca64ee226060d3e2e82d4
LeoMetallic <lmetalli@iu.edu>
2018-09-14 13:16:43 -0400

    Code for Assignment. (Formatting is needed)

---
+++ Public/JS/aboutme.js
@@ -0,0 +1,47 @@
+var RandomUser = new Vue ({
+    el: '#bio',
+    data: {
+      person: {
+            "name": '',
+            "origin": '',
+            "dob": '',
+            "gender": '',
+            "img": '',
+            "email":''
+          },
+      },
+
+//anumalho@iu.edu
+
+methods: {
+  prettyDate: function (d) {
+    return moment(d).format('l')
+  },
+
+  calculateAge(thiswouldbeeasierifwepulledAgefromRandomUser) {
+    return moment().diff(thiswouldbeeasierifwepulledAgefromRandomUser, 'years');
+  },
+
+  fetchRandomUser () {
+    fetch('https://randomuser.me/api')
+    .then( response => response.json() )
+    .then( json =>
+      {RandomUser.person.name = json.results[0].name.first+" "+json.results[0].name.last,
+        RandomUser.person.gender=json.results[0].gender,
+        RandomUser.person.origin=json.results[0].nat,
+        RandomUser.person.dob=json.results[0].dob.date,
+
+        RandomUser.person.img=json.results[0].picture.large,
+        RandomUser.person.email=json.results[0].email
+        } )
+    .catch( err => {
+      console.log('TASK FETCH ERROR:');
+      console.log(err);
+    })
+  },
+},
+created()
+{
+  this.fetchRandomUser();
+}
+});
