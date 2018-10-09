4be9d47eeea2571b1a3ca2cd707d8af21025e544
LeoMetallic <lmetalli@iu.edu>
2018-10-09 12:00:44 -0400

    Bringing in fetchAll and create methods

--- Public/api/comment.php
+++ Public/api/comment.php
@@ -1,9 +1,17 @@
 <?php
 require '../../app/common.php';
 // 1. Go to the database and get all teams
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $work = new Work($_POST);
+  $work->create();
+  echo json_encode($work);
+  exit;
+}
 $comments = Comment::fetchAll();
 // 2. Convert to JSON
 $json = json_encode($comments, JSON_PRETTY_PRINT);
 // 3. Print
 header('Content-Type: application/json');
 echo $json;
+
+//change work to comment
--- app/models/comment.php
+++ app/models/comment.php
@@ -4,11 +4,39 @@ class Comment
   public $id;
   public $comment;
 
-public static function fetchAll() {
-  // 1. Connect to the database
-  // 2. Prepare the query
-  // 3. Run the query
-  // 4. Handle the results
+//just need id and comment
+  public function create() {
+  $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+  $sql = 'INSERT INTO Work (team_id, task_id, start_date, hours, completion_estimate)
+          VALUES (?,?,?,?,?)';
+  $statement = $db->prepare($sql);
+  $success = $statement->execute([
+    $this->team_id,
+    $this->task_id,
+    $this->start,
+    $this->hours,
+    $this->completion_estimate
+  ]);
+  $this->id = $db->lastInsertId();
 }
-return $arr;
-}
+
+  public static function fetchAll() {
+    // 1. Connect to the database
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+
+    // 2. Prepare the query
+    $sql = 'SELECT * FROM Teams';
+    $statement = $db->prepare($sql);
+
+    // 3. Run the query
+    $success = $statement->execute();
+
+    // 4. Handle the results
+    $arr = [];
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+      $theTeam =  new Team($row);
+      array_push($arr, $theTeam);
+    }
+    return $arr;
+  }
+//change Teams to Comment
