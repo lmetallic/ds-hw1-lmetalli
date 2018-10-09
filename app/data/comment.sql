c17776bccee0be96a92fdad89ee4fcd5e3455d5e
LeoMetallic <lmetalli@iu.edu>
2018-10-09 14:24:47 -0400

    id-->comment_id

--- app/data/comment.sql
+++ app/data/comment.sql
@@ -3,11 +3,11 @@ DROP TABLE IF EXISTS Comment;
 
 CREATE TABLE Comment
 (
-  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  comment_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   comment varchar(140) NOT NULL
 );
 
 INSERT INTO Comment (comment) VALUES ('You eat out too much. -MOM'),
 ('You eat out too much. -DAD'), ('You should try out this new restaurant. -ME');
 
-SELECT * FROM Comments;
+SELECT * FROM Comment;
