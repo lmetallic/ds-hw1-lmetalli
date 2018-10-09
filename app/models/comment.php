220f1fe2b51a75d53b873934dcd0a50475767c71
LeoMetallic <lmetalli@iu.edu>
2018-10-08 20:31:44 -0400

    File creation for HW #6

---
+++ app/models/comment.php
@@ -0,0 +1,14 @@
+<?php
+class Comment
+{
+  public $id;
+  public $comment;
+
+public static function fetchAll() {
+  // 1. Connect to the database
+  // 2. Prepare the query
+  // 3. Run the query
+  // 4. Handle the results
+}
+return $arr;
+}
